b524911dfca72b6d34b23f3d1911d6886b235689
litefeel <litefeel@gmail.com>
2019-05-22 21:59:36 +0800

    add $UNITY_PROJECT_DIR

--- ci/docker_test.sh
+++ ci/docker_test.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+if [ -z "$UNITY_PROJECT_DIR" ]; then
+  UNITY_PROJECT_DIR=$(pwd)
+fi
+
+echo "UNITY_PROJECT_DIR:$UNITY_PROJECT_DIR"
+
 docker run \
   -e UNITY_LICENSE_CONTENT \
   -e TEST_PLATFORM \
   -e UNITY_USERNAME \
   -e UNITY_PASSWORD \
   -w /project/ \
-  -v $(pwd):/project/ \
+  -v $UNITY_PROJECT_DIR:/project/ \
   $IMAGE_NAME \
   /bin/bash -c "/project/ci/before_script.sh && /project/ci/test.sh"
